d51b9df95b38db65deb88b5571f14f710800fea6
Jonathan Allen <jallen@snow.edu>
2021-11-17 08:04:44 -0700

    Exam 3 study guide

--- Code/Program.cs
+++ Code/Program.cs
@@ -1,12 +1,61 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Code
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
         }
+
+
+        public static decimal CircleDiameter(decimal area)
+        {
+            //you write this code
+            throw new NotImplementedException();
+        }
+
+        public static decimal RightTriangleSideLength(decimal side1, decimal angle, decimal side2)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static List<string> fileContents = new List<string>(){
+            "Sally|62",
+            "Frank|75",
+            "Jessica|80",
+            "Mary|95",
+            "Henry|93",
+            "Todd|97",
+            "Hank|89",
+            "Jane|95"
+        };
+
+        public static Results ProcessTestScores()
+        {
+            //Assign these values as you go through each line in the "fileName" file
+            decimal averageScore = -1;
+            decimal maxScore = -1;
+            string maxScoreName = null;
+            decimal minScore = -1;
+            string minScoreName = null;
+
+            foreach(var line in fileContents)
+            {
+
+            }
+            
+            return new Results(averageScore, maxScore, maxScoreName, minScore, minScoreName);
+        }
     }
+
+    public record Results(
+        decimal AverageScore,
+        decimal MaxScore,
+        string MaxScoreName,
+        decimal MinScore,
+        string MinScoreName
+    );
 }
--- Tests/UnitTest1.cs
+++ Tests/UnitTest1.cs
@@ -1,18 +1,101 @@
+using System;
+using Code;
 using NUnit.Framework;
 
 namespace Tests
 {
-    public class Tests
+    public class CircleDiameterTests
     {
-        [SetUp]
-        public void Setup()
+        [Test]
+        public void FindDiameter()
         {
+            var actual = Program.CircleDiameter(78.53975M);
+            var expected = 10;
+            Assert.AreEqual(expected, actual);
         }
 
         [Test]
-        public void Test1()
+        public void AreaOfZeroThrowsException()
         {
-            Assert.Pass();
+            try
+            {
+                var actual = Program.CircleDiameter(0M);
+                Assert.Fail("No exception was thrown");
+            }
+            catch (Exception ex)
+            {
+                Assert.AreEqual("Area must be > 0", ex.Message);
+            }
+        }
+
+        [Test]
+        public void NegativeAreaThrowsException()
+        {
+            try
+            {
+                var actual = Program.CircleDiameter(-1);
+                Assert.Fail("No exception was thrown");
+            }
+            catch (Exception ex)
+            {
+                Assert.AreEqual("Area must be > 0", ex.Message);
+            }
+        }
+
+        [Test]
+        public void ValidSideLength()
+        {
+            var actual = Program.RightTriangleSideLength(3, 90, 4);
+            var expected = 5;
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void InvalidAngle()
+        {
+            try
+            {
+                var actual = Program.RightTriangleSideLength(3, 45, 4);
+            }
+            catch (Exception ex)
+            {
+                Assert.AreEqual("Only works with right angles", ex.Message);
+            }
+        }
+
+        [Test]
+        public void AverageScore()
+        {
+            var actualResult = Program.ProcessTestScores();
+            Assert.AreEqual(85.75M, actualResult.AverageScore);
+        }
+        
+        [Test]
+        public void MaxScore()
+        {
+            var actualResult = Program.ProcessTestScores();
+            Assert.AreEqual(97, actualResult.MaxScore);
+        }
+                
+        [Test]
+        public void MaxScoreName()
+        {
+            var actualResult = Program.ProcessTestScores();
+            Assert.AreEqual("Todd", actualResult.MaxScoreName);
+        }        
+
+        [Test]
+        public void MinScore()
+        {
+            var actualResult = Program.ProcessTestScores();
+            Assert.AreEqual(62, actualResult.MinScore);
+        }
+                
+        [Test]
+        public void MinScoreName()
+        {
+            var actualResult = Program.ProcessTestScores();
+            Assert.AreEqual("Sally", actualResult.MinScoreName);
         }
     }
 }
